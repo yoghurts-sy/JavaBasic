9551401a70a19bfe480742cfb0509f3e97aeffcb
unknown <yoghurts_sy@sina.com>
2020-04-09 08:59:43 +0800

    2020/4/9

---
+++ src/com/Java/Generic/InterfaceImp1.java
@@ -0,0 +1,22 @@
+package com.Java.Generic;
+/*
+ * 接口实现类1:第一种方法
+ * 直接规定接口的类型
+ * 格式：
+ *     public class imp implements Interface<E>{
+ *     }
+ */
+public class InterfaceImp1 implements GenericInterface<String>{
+
+	@Override
+	public void method(String e) {
+		System.out.println(e);
+		
+	}
+
+	@Override
+	public String get(String e) {
+		return e;
+	}
+	
+}
---
+++ src/com/Java/Generic/InterfaceImp2.java
@@ -0,0 +1,23 @@
+package com.Java.Generic;
+/*
+ * 带泛型的接口第二种使用方法：
+ * 接口使用什么泛型，实现类就使用什么泛型
+ * 格式：
+ *     public class imp<E> implements Interface<E>{
+ *     }
+ */
+public class InterfaceImp2<E> implements GenericInterface<E>{
+
+	@Override
+	public void method(E e) {
+		System.out.println(e);
+		
+	}
+
+	@Override
+	public E get(E e) {
+		return e;
+	}
+
+	
+}
