13a897144e89c8e61c3d6ab115c408ba916a8ebb
unknown <yoghurts_sy@sina.com>
2020-05-07 14:31:38 +0800

    2020/5/7

--- src/IOAndFile/IO_04_Writer/Demo01Writer.java
+++ src/IOAndFile/IO_04_Writer/Demo01Writer.java
@@ -1,5 +1,8 @@
 package IOAndFile.IO_04_Writer;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /** 字符输出流：Writer
  *  `java.io.Writer ：
  *  `抽象类是表示用于写出字符流的所有类的超类，将指定的字符信息写出到目的地。
@@ -22,6 +25,18 @@ package IOAndFile.IO_04_Writer;
  *          2.会根据构造方法中传递的文件/文件的路径，创建文件
  *          3.会把FileWriter对象指向创建好的文件
  *
+ *  字符输出流使用步骤（重点）
+ *      1.创建FileWriter对象，构造方法中绑定要写入的数据目的地
+ *      2.使用FileWriter中的write方法，把数据写入内存的缓存区（字符转化为字节）
+ *      3.使用FileWriter中的flush方法，把内存缓冲区中的数据，刷新到文件中
+ *      4.释放资源（如果没使用flush方法，也会把内存缓存区中的数据刷新到文件中）
+ *
  */
 public class Demo01Writer {
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("C:\\Users\\yoghurts\\IDEA_WorkSpace\\com.java.basic\\src\\IOAndFile\\IO_04_Writer\\test.txt");
+        fw.write(97);
+        fw.flush();
+        fw.close();
+    }
 }
