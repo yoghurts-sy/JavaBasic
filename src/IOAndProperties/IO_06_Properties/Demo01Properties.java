2a0116b408c16f0e218f76947c19d0918db4b2e4
unknown <yoghurts_sy@sina.com>
2020-05-09 14:32:53 +0800

    2020/5/9

--- src/IOAndProperties/IO_06_Properties/Demo01Properties.java
+++ src/IOAndProperties/IO_06_Properties/Demo01Properties.java
@@ -1,5 +1,6 @@
 package IOAndProperties.IO_06_Properties;
 
+import java.io.*;
 import java.util.Properties;
 import java.util.Set;
 
@@ -13,10 +14,20 @@ import java.util.Set;
  *      - `public Set<String> stringPropertyNames() ` ：所有键的名称的集合。
  *
  *      泛型都是默认字符串
+ *
+ *      可以使用Properties集合中的方法store，把集合中的临时数据写入硬盘中存储
+ *      store(OutputStream out, String comments)
+ *      store(Writer writer, String comments)
+ *      参数：
+ *          OutputStream out：字节输出流，不能写入中文
+ *          Writer writer：字符输出流，可以写中文
+ *          String comments:注释，不能使用中文，因为默认Unicode编码
  */
 public class Demo01Properties {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         show01();
+        show02();
+        show03();
     }
 
     private static void show01() {
@@ -31,5 +42,30 @@ public class Demo01Properties {
             String value = properties.getProperty(key);
             System.out.println(key + value);
         }
+    }
+
+    private static void show02() throws IOException {
+        Properties properties = new Properties();
+
+        properties.setProperty("赵丽颖","170");
+        properties.setProperty("迪丽热巴","171");
+        properties.setProperty("马尔扎哈","Infinity");
+
+        FileWriter fw  =  new FileWriter("C:\\Users\\yoghurts\\IDEA_WorkSpace\\com.java.basic\\src\\IOAndProperties\\IO_06_Properties\\prop.txt");
+        properties.store(fw ,"2020/5/9");
+
+        fw.close();
+
+    }
+
+    private static void show03() throws IOException {
+        Properties properties = new Properties();
+
+        properties.setProperty("赵丽颖","170");
+        properties.setProperty("迪丽热巴","171");
+        properties.setProperty("马尔扎哈","Infinity");
+
+        properties.store(new FileOutputStream("C:\\Users\\yoghurts\\IDEA_WorkSpace\\com.java.basic\\src\\IOAndProperties\\IO_06_Properties\\prop2.txt"), "2020/5/9");
+        //会自动释放资源
     }
 }
